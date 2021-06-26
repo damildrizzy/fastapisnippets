96df7270605b041fa547c0b2449d21a5a604ee41
= <=>
2021-06-26 23:13:59 +0100

    inclue routers

--- server/app/app/main.py
+++ server/app/app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 
+from app.api import auth, users
+from .database import engine, Base
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.include_router(auth.router, prefix="/auth", tags=["auth"])
+app.include_router(users.router, prefix="/users", tags=["users"])
