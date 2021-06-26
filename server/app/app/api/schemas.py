c0d644226b7f233d25a37a832f0f2716735092f7
= <=>
2021-06-26 23:14:28 +0100

    add user schemas

---
+++ server/app/app/api/schemas.py
@@ -0,0 +1,30 @@
+from typing import Optional
+from pydantic import BaseModel
+
+
+class UserBase(BaseModel):
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenPayload(BaseModel):
+    sub: Optional[str] = None
+
+
+class TongueBase(BaseModel):
+    raw_string: str
