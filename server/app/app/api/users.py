5037f8d71079ab865f3972357bf7528af5feb96c
= <=>
2021-06-26 17:55:05 +0100

    user endpoints

---
+++ server/app/app/api/users.py
@@ -0,0 +1,33 @@
+from fastapi import Depends, HTTPException
+from fastapi.routing import APIRouter
+from sqlalchemy.orm import Session
+from typing import List
+
+
+from . import schemas, crud
+from .deps import get_db
+
+
+router = APIRouter()
+
+
+@router.post("/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        return HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
+
+
+@router.get("/", response_model=List[schemas.User])
+def list_users(db: Session = Depends(get_db)):
+    users = crud.get_users(db)
+    return users
+
+
+@router.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
