239c350e098e26c697148447d9617d66a980f289
= <=>
2021-06-26 17:47:57 +0100

    user model

---
+++ server/app/app/models/user.py
@@ -0,0 +1,10 @@
+from app.database import Base
+from sqlalchemy import Column, String, Boolean, Integer
+
+class User(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean(), default=True)
+    is_superuser = Column(Boolean(), default=False)
