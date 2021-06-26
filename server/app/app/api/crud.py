3701114804c058d668c158cea5ba9cef5c1f4ee4
= <=>
2021-06-26 18:07:19 +0100

    crud operations

---
+++ server/app/app/api/crud.py
@@ -0,0 +1,37 @@
+from typing import Optional
+
+from sqlalchemy.orm import Session
+
+from . import schemas
+from app.models.user import User
+from app.security import get_password_hash, verify_password
+
+
+def get_user(db: Session, email: int):
+    return db.query(User).filter(User.email == email).first()
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+
+def get_users(db: Session):
+    return db.query(User).all()
+
+
+def create_user(db: Session, user: schemas.UserCreate) -> User:
+    hashed_password = get_password_hash(user.password)
+    db_user = User(email=user.email, hashed_password=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def authenticate(db: Session, email: str, password: str) -> Optional[User]:
+    user = get_user_by_email(db, email=email)
+    if not user:
+        return None
+    if not verify_password(password, user.hashed_password):
+        return None
+    return user
