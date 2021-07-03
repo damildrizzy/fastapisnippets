e261fa164fad105020b9694200e13b4acae98026
samuel adekoya <damiloladrizzy@yahoo.com>
2021-07-03 22:44:51 +0100

    alembic files

--- server/app/app/api/crud.py
+++ server/app/app/api/crud.py
@@ -21,7 +21,7 @@ def get_users(db: Session):
 
 def create_user(db: Session, user: schemas.UserCreate) -> User:
     hashed_password = get_password_hash(user.password)
-    db_user = User(email=user.email, hashed_password=hashed_password)
+    db_user = User(email=user.email, username=user.username, full_name=user.full_name, hashed_password=hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
