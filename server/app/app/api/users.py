1e3b8d506253c287bc4bdb8dfce279206dfd2ec7
samuel adekoya <damiloladrizzy@yahoo.com>
2021-07-09 21:58:44 +0100

    refactoring

--- server/app/app/api/users.py
+++ server/app/app/api/users.py
@@ -3,29 +3,30 @@ from fastapi.routing import APIRouter
 from sqlalchemy.orm import Session
 from typing import List
 
-from . import schemas, crud
+from ..crud.user import create_user, get_user_by_email, get_users, get_user
+from ..schemas import auth
 from .deps import get_db
 
 router = APIRouter()
 
 
-@router.post("/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
+@router.post("/", response_model=auth.User)
+def create_user(user: auth.UserCreate, db: Session = Depends(get_db)):
+    db_user = get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
+    return create_user(db=db, user=user)
 
 
-@router.get("/", response_model=List[schemas.User])
+@router.get("/", response_model=List[auth.User])
 def list_users(db: Session = Depends(get_db)):
-    users = crud.get_users(db)
+    users = get_users(db)
     return users
 
 
-@router.get("/users/{user_id}", response_model=schemas.User)
+@router.get("/users/{user_id}", response_model=auth.User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
+    db_user = get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
