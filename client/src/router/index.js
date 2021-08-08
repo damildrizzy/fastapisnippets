240ae689f011717e8753cb0914b80e583a4d072e
samuel adekoya <damiloladrizzy@yahoo.com>
2021-08-08 18:51:54 +0100

    implement code highlighting feature

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Register from "../views/Register.vue";
+import SnippetCreate from "@/views/SnippetCreate";
 
 const routes = [
   {
@@ -21,6 +22,11 @@ const routes = [
     path: "/register",
     name: "Register",
     component: Register,
+  },
+  {
+    path: "/create",
+    name: "SnippetCreate",
+    component: SnippetCreate,
   },
 ];
 
---
+++ client/src/views/SnippetCreate.vue
@@ -0,0 +1,57 @@
+<template>
+  <prism-editor
+    class="my-editor"
+    v-model="code"
+    :highlight="highlighter"
+    line-numbers
+  ></prism-editor>
+</template>
+
+<script>
+import { PrismEditor } from "vue-prism-editor";
+import "vue-prism-editor/dist/prismeditor.min.css"; // import the styles somewhere
+
+// import highlighting library (you can use any library you want just return html string)
+import { highlight, languages } from "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-javascript";
+import "prismjs/components/prism-python";
+import "prismjs/themes/prism-tomorrow.css"; // import syntax highlighting styles
+
+export default {
+  name: "SnippetCreate",
+  components: {
+    PrismEditor,
+  },
+  data() {
+    return {
+      code: 'print("Hello World")',
+    };
+  },
+  methods: {
+    highlighter() {
+      return highlight(this.code, languages.py);
+    },
+  },
+};
+</script>
+
+<style>
+/* required class */
+.my-editor {
+  /* we dont use `language-` classes anymore so that's why we need to add background and text color manually */
+  background: #2d2d2d;
+  color: #ccc;
+
+  /* you must provide font-family font-size line-height. Example: */
+  font-family: Fira code, Fira Mono, Consolas, Menlo, Courier, monospace;
+  font-size: 14px;
+  line-height: 1.5;
+  padding: 5px;
+}
+
+/* optional class for removing the outline */
+.prism-editor__textarea:focus {
+  outline: none;
+}
+</style>
