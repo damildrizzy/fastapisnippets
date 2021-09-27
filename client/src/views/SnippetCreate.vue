66adfd2baa923b77af49ff3f50160db54338832c
samuel adekoya <damiloladrizzy@yahoo.com>
2021-09-27 20:31:59 +0100

    ui and button

--- client/src/views/SnippetCreate.vue
+++ client/src/views/SnippetCreate.vue
@@ -2,81 +2,133 @@
   <div class="px-6 py-8 bg-gray-100">
     <div class="lg:w-9/12">
       <div class="flex items-center justify-between">
-    <h1 class="text-xl font-bold text-gray-700 md:text-2xl">Post A Snippet</h1>
-    </div>
-    <div class="bg-white rounded p-5 mt-6">
-      <Form class="w-full mt-5">
-        <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full px-3">
-            <label
-              class="
-                block
-                uppercase
-                tracking-wide
-                float-left
-                text-gray-700 text-xs
-                font-bold
-                mb-2
-              "
-              for="grid-title"
-            >
-              Title
-            </label>
-            <Field
-              name="title"
-              class="
-                appearance-none
-                block
-                w-full
-                text-gray-700
-                border border-gray-200
-                rounded
-                py-3
-                px-4
-                mb-3
-                leading-tight
-                focus:outline-none focus:bg-white focus:border-gray-500
-              "
-              id="grid-title"
-              type="text"
-            ></Field>
-          </div>
-        </div>
-        <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full px-3">
-            <div>
-                <label
+        <h1 class="text-xl font-bold text-gray-700 md:text-2xl">
+          Post A Snippet
+        </h1>
+      </div>
+      <div class="bg-white rounded p-5 mt-6">
+        <Form class="w-full mt-5">
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full px-3">
+              <label
                 class="
                   block
                   uppercase
                   tracking-wide
+                  float-left
                   text-gray-700 text-xs
                   font-bold
                   mb-2
                 "
-                for="grid-description"
+                for="grid-title"
               >
-                Description
+                Title
               </label>
+              <Field
+                name="title"
+                class="
+                  appearance-none
+                  block
+                  w-full
+                  text-gray-700
+                  border border-gray-200
+                  rounded
+                  py-3
+                  px-4
+                  mb-3
+                  leading-tight
+                  focus:outline-none focus:bg-white focus:border-gray-500
+                "
+                id="grid-title"
+                type="text"
+              ></Field>
             </div>
-           <vue-editor v-model="description" id="grid-description"></vue-editor>  
           </div>
-        </div>
-        
-        <div>
-          <prism-editor
-          class="my-editor overflow-auto h-96"
-          v-model="code"
-          :highlight="highlighter"
-          line-numbers>
-        </prism-editor>
-        </div>
-        
-      </Form>
-      
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full px-3">
+              <div>
+                <label
+                  class="
+                    block
+                    uppercase
+                    tracking-wide
+                    text-gray-700 text-xs
+                    font-bold
+                    mb-2
+                  "
+                  for="grid-description"
+                >
+                  Description
+                </label>
+              </div>
+              <vue-editor
+                v-model="description"
+                id="grid-description"
+              ></vue-editor>
+            </div>
+          </div>
+          <div class="mb-6">
+            <prism-editor
+              class="my-editor overflow-auto h-96"
+              v-model="code"
+              :highlight="highlighter"
+              line-numbers
+            >
+            </prism-editor>
+          </div>
+          <div class="flex flex-wrap -mx-3">
+            <div class="w-full px-3">
+              <label
+                class="
+                  block
+                  uppercase
+                  tracking-wide
+                  float-left
+                  text-gray-700 text-xs
+                  font-bold
+                  mb-2
+                "
+                for="grid-tags"
+              >
+                Tags
+              </label>
+              <Field
+                name="tags"
+                class="
+                  appearance-none
+                  block
+                  w-full
+                  text-gray-700
+                  border border-gray-200
+                  rounded
+                  py-3
+                  px-4
+                  mb-3
+                  leading-tight
+                  focus:outline-none focus:bg-white focus:border-gray-500
+                "
+                id="grid-tags"
+                type="text"
+              ></Field>
+            </div>
+          </div>
+        </Form>
+      </div>
     </div>
-    </div>
-    
+    <button
+      class="
+        bg-green-500
+        hover:bg-green-700
+        text-white
+        font-bold
+        mt-6
+        py-2
+        px-4
+        rounded
+      "
+    >
+      Post Snippet
+    </button>
   </div>
 </template>
 
@@ -104,7 +156,7 @@ export default {
   data() {
     return {
       code: "# code snippet goes here",
-      description: ""
+      description: "",
     };
   },
   methods: {
@@ -127,10 +179,5 @@ export default {
   font-size: 14px;
   line-height: 1.5;
   padding: 5px;
-}
-
-/* optional class for removing the outline */
-.prism-editor__textarea:focus {
-  outline: none;
 }
 </style>
