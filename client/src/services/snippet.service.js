9660f07c2d2b47cdde6d227210064d4f2230ad9a
samuel adekoya <damiloladrizzy@yahoo.com>
2021-10-27 20:25:21 +0100

    add createSnippet service

--- client/src/services/snippet.service.js
+++ client/src/services/snippet.service.js
@@ -1,10 +1,21 @@
 import axios from "axios";
+import authHeader from "./auth-header";
 
 const API_URL = "http://localhost:8000/snippets";
 
 class SnippetService {
   getSnippets() {
     return axios.get(API_URL);
+  }
+
+  createSnippet(snippet) {
+    const data = {
+      title: snippet.title,
+      description: snippet.description,
+      code: snippet.code,
+    };
+
+    return axios.post(API_URL, data, { headers: authHeader() });
   }
 
   getTopAuthors() {
