5d62306fd5f6a15cf6477b9e546a94c7013bdca1
samuel adekoya <damiloladrizzy@yahoo.com>
2021-11-01 17:26:41 +0100

    migrations

---
+++ server/app/alembic/versions/bef7e4966f64_update_table_names.py
@@ -0,0 +1,24 @@
+"""update table names
+
+Revision ID: bef7e4966f64
+Revises: 61477ca1d0a3
+Create Date: 2021-11-01 16:21:44.278352
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bef7e4966f64'
+down_revision = '61477ca1d0a3'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
