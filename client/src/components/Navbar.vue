42efa28204daa36514ce3cc70ea3915d035da780
damildrizzy <damiloladrizzy@yahoo.com>
2022-04-15 08:01:24 +0100

    refactor navbar

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav
+      v-if="!['Login', 'Register'].includes($route.name)"
+      class="flex items-center p-6"
+  >
+    <div class="container flex justify-between mx-auto items-center text-teal">
+      <div class="w-full lg:w-6/12">
+        <router-link class="font-semibold text-xl tracking-tight" to="/"
+        >FastAPI Snippets
+        </router-link>
+      </div>
+
+      <div class="w-full lg:w-6/12 block flex-grow lg:flex lg:w-auto">
+        <div class="text-lg lg:flex-grow text-right">
+          <router-link
+              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 mr-4"
+              to="/create"
+          >
+            Create Snippet
+          </router-link>
+          <router-link
+              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 mr-4"
+              to="/login"
+          >
+            Login
+          </router-link>
+          <router-link
+              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 mr-4"
+              to="/register"
+          >Register
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
