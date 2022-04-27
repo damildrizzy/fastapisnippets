f21b518e432655894c3fd10b295bfcd68b360d6a
damildrizzy <damiloladrizzy@yahoo.com>
2022-04-27 01:42:56 +0100

    refactor env vars

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -1,34 +1,34 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000";
+const API_URL = process.env.API_BASE_URL;
 
 class AuthService {
-  login(user) {
-    return axios
-      .post(`${API_URL}/auth/access-token`, {
-        identifier: user.identifier,
-        password: user.password,
-      })
-      .then((response) => {
-        if (response.data["access_token"]) {
-          localStorage.setItem("user", JSON.stringify(response.data));
-        }
-        return response.data;
-      });
-  }
+    login(user) {
+        return axios
+            .post(`${API_URL}/auth/access-token`, {
+                identifier: user.identifier,
+                password: user.password,
+            })
+            .then((response) => {
+                if (response.data["access_token"]) {
+                    localStorage.setItem("user", JSON.stringify(response.data));
+                }
+                return response.data;
+            });
+    }
 
-  logout() {
-    localStorage.removeItem("user");
-  }
+    logout() {
+        localStorage.removeItem("user");
+    }
 
-  register(user) {
-    return axios.post(`${API_URL}/users`, {
-      full_name: user.name,
-      username: user.username,
-      email: user.email,
-      password: user.password,
-    });
-  }
+    register(user) {
+        return axios.post(`${API_URL}/users`, {
+            full_name: user.name,
+            username: user.username,
+            email: user.email,
+            password: user.password,
+        });
+    }
 }
 
 export default new AuthService();
--- client/src/services/snippet.service.js
+++ client/src/services/snippet.service.js
@@ -1,31 +1,31 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = "http://localhost:8000/snippets";
+const API_URL = `${process.env.API_BASE_URL}/snippets`;
 
 class SnippetService {
-  getSnippets() {
-    return axios.get(API_URL);
-  }
+    getSnippets() {
+        return axios.get(API_URL);
+    }
 
-  getSnippet(id) {
-    return axios.get(`${API_URL}/${id}`);
-  }
+    getSnippet(id) {
+        return axios.get(`${API_URL}/${id}`);
+    }
 
-  createSnippet(snippet) {
-    const data = {
-      title: snippet.title,
-      description: snippet.description,
-      code: snippet.code,
-      tags: snippet.tags,
-    };
+    createSnippet(snippet) {
+        const data = {
+            title: snippet.title,
+            description: snippet.description,
+            code: snippet.code,
+            tags: snippet.tags,
+        };
 
-    return axios.post(API_URL, data, { headers: authHeader() });
-  }
+        return axios.post(API_URL, data, {headers: authHeader()});
+    }
 
-  getTopAuthors() {
-    return axios.get(`${API_URL}/top-authors`);
-  }
+    getTopAuthors() {
+        return axios.get(`${API_URL}/top-authors`);
+    }
 }
 
 export default new SnippetService();
