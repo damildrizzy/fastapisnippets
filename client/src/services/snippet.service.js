9931443a45f55e9c511f595268a3b9d8ab121db3
damildrizzy <damiloladrizzy@yahoo.com>
2022-04-28 12:46:51 +0100

    Resolve vue env var issue

--- client/src/services/snippet.service.js
+++ client/src/services/snippet.service.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import authHeader from "./auth-header";
 
 const API_URL = `${process.env.VUE_APP_API_BASE_URL}/snippets`;
-console.log(API_URL)
 
 class SnippetService {
     getSnippets() {
